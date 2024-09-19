ce6f32f541f8eb330627c68a60d944c1e1514d41
Dallacort <129295431+otaviodallacort@users.noreply.github.com>
2024-09-19 09:33:26 -0300

    Add files via upload

--- ADV.js
+++ ADV.js
@@ -1,11 +1,19 @@
 function cadastra_aluno() {
-    const no_al= document.getElementById('nome_aluno').value;
-    const dn_al= document.getElementById('data_nascimento').value;
-    const cg_al= document.getElementById('n_processo').value;
+    const no_al= document.getElementById('mostra_nome').value;
+    const dn_al= document.getElementById('mostra_data').value;
+    const no_tl= document.getElementById('mostra_telefone').value;
+    const no_cp= document.getElementById('mostra_cpf').value;
+    const dn_rg= document.getElementById('mostra_rg').value;
+    const no_cep= document.getElementById('mostra_cep').value;
 
     document.getElementById('mostra_nome').innerText= no_al;
     document.getElementById('mostra_data').innerText= dn_al;
-    document.getElementById('mostra_cgm').innerText= cg_al;
+    document.getElementById('mostra_telefone').innerText= no_tl;
+    document.getElementById('mostra_cpf').innerText= no_cpf;
+    document.getElementById('mostra_rg').innerText= no_rg;
+    document.getElementById('mostra_cep').innerText= no_cep;
+
+    
 
 
     
