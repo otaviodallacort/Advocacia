67d35382b8db1ec17848c50d267beef6503c1b08
Dallacort <129295431+otaviodallacort@users.noreply.github.com>
2024-09-24 08:48:09 -0300

    Add files via upload

--- ADV.js
+++ ADV.js
@@ -6,14 +6,6 @@ function cadastra_aluno() {
     const dn_rg= document.getElementById('mostra_rg').value;
     const no_cep= document.getElementById('mostra_cep').value;
 
-    document.getElementById('mostra_nome').innerText= no_al;
-    document.getElementById('mostra_data').innerText= dn_al;
-    document.getElementById('mostra_telefone').innerText= no_tl;
-    document.getElementById('mostra_cpf').innerText= no_cpf;
-    document.getElementById('mostra_rg').innerText= no_rg;
-    document.getElementById('mostra_cep').innerText= no_cep;
-
-    
 
 
     
